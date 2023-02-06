8b0414cfd368605b817bbabb6d85cfa83c42c49e
Covadonga M <cova.millan@gmail.com>
2023-02-06 20:22:03 +0100

    Pass add test

--- src/components/component/app/App.tsx
+++ src/components/component/app/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Gentleman } from "../../gentleman/gentleman";
+import { GENTLEMEN } from "../../../mocks/mock";
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <header className="main-header">
+          <h1 className="main-title">The pointing gentlemen</h1>
+        </header>
+        <section className="controls">
+          <p className="info">0 gentlemen pointing at you</p>
+          <button className="button button--select">Select all</button>
+        </section>
+      </div>
+      <main className="main">
+        <ul className="gentlemen">
+          {GENTLEMEN.map((elem) => (
+            <Gentleman
+              name={elem.name}
+              status={elem.status}
+              profession={elem.profession}
+              twitter={elem.twitter}
+              picture={elem.picture}
+              alternativeText={elem.alternativeText}
+            ></Gentleman>
+          ))}
+        </ul>
+      </main>
+    </>
   );
 }
 
