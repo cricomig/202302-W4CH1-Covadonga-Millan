cdb50b40e57e9dcf9ccdaa15aeb1aa38c5c866c7
Covadonga M <cova.millan@gmail.com>
2023-02-06 20:46:35 +0100

    Update app test and tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,33 +2,22 @@ import React from "react";
 import "./App.css";
 import { Gentleman } from "../gentleman/gentleman";
 import { GENTLEMEN } from "../../mocks/mock";
+import { Info } from "../info/info";
 
 export function App() {
   return (
     <>
-      <div className="container">
-        <header className="main-header">
-          <h1 className="main-title">The pointing gentlemen</h1>
-        </header>
-        <section className="controls">
-          <p className="info">0 gentlemen pointing at you</p>
-          <button className="button button--select">Select all</button>
-        </section>
-      </div>
-      <main className="main">
-        <ul className="gentlemen">
-          {GENTLEMEN.map((elem) => (
-            <Gentleman
-              name={elem.name}
-              status={elem.status}
-              profession={elem.profession}
-              twitter={elem.twitter}
-              picture={elem.picture}
-              alternativeText={elem.alternativeText}
-            ></Gentleman>
-          ))}
-        </ul>
-      </main>
+      <Info></Info>
+      {GENTLEMEN.map((elem) => (
+        <Gentleman
+          name={elem.name}
+          status={elem.status}
+          profession={elem.profession}
+          twitter={elem.twitter}
+          picture={elem.picture}
+          alternativeText={elem.alternativeText}
+        ></Gentleman>
+      ))}
     </>
   );
 }
